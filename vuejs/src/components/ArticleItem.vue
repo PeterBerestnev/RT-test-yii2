a5f6519bab27144e1667bf15c6cc30ce8d903c7d
Peter <Petkadrifter@gmail.com>
2023-04-09 15:54:38 +0400

    pre-load version

--- vuejs/src/components/ArticleItem.vue
+++ vuejs/src/components/ArticleItem.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="card bg-light ">
         <div class="card-header">
-            <div class="d-flex flex-row justify-content-between" v-if="isAuth && this.$route.name == 'admin-panel-main'">
+            <div class="d-flex flex-row justify-content-between" v-if="isAuth">
                 <router-link :to="{ name: 'admin-update-article', query: { id: post._id } }">
                     <h2>
                         <strong>
@@ -9,7 +9,8 @@
                         </strong>
                     </h2>
                 </router-link>
-                <div @click="deleteItem" class="fa-solid fa-xmark mt-2 fs-3"></div>
+                <div @click="deleteItem" v-if="this.$route.name == 'admin-panel-main'" class="fa-solid fa-xmark mt-2 fs-3"></div>
+                <div v-else-if="this.$route.name == 'admin-popular'" class="fa-solid fa-eye ms-auto align-self-center">{{ post.views }}</div>
             </div>
             <div v-else>
                 <router-link :to="{ name: 'article-main', query: { id: post._id } }">
@@ -41,7 +42,7 @@ export default {
     },
     data(){
         return{
-            isAuth: false
+            isAuth: false,
         }
     },
     methods:{
